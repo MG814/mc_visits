fe60066c755a715a31144b07f34007c74bbd888d
MICHAŁ <mg814@wp.pl>
2024-09-04 08:38:11 +0200

    ci: add new environment variables

--- src/core/settings.py
+++ src/core/settings.py
@@ -22,13 +22,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f)3l311h+7$hffk=v03-99bcpv@4_=q5e+$&#5x0vh1j^6-%n)'
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
