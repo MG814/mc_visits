92759598f689720432aedb14726073913374259d
MICHAŁ <mg814@wp.pl>
2025-06-27 17:54:23 +0200

    refactor: remove uninstalled package

--- src/core/settings.py
+++ src/core/settings.py
@@ -15,7 +15,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'rest_framework',
-    'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
